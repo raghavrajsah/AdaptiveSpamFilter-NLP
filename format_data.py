3336ac60f6a3f7f2969b2a0b01bee749af23e2f0
mburack <123586611+mburack@users.noreply.github.com>
2024-12-16 19:09:12 -0500

    Add files via upload

--- format_data.py
+++ format_data.py
@@ -1,24 +1,21 @@
 import pandas as pd
 
-column_names = ["labels", "text", "text_hi", "text_de", "text_fr"]
-df = pd.read_csv('data-en-hi-de-fr 2.csv', names=column_names, skiprows=1)
+reference_df = pd.read_csv('data-en-hi-de-fr 2.csv')
 
-label_mapping = {'ham': 0, 'spam': 1}
-
-df_final = pd.DataFrame(columns=['textid', 'text', 'label'])
-
-idx = 0
 rows_to_add = []
-for _, row in df.iterrows():
-    rows_to_add.append({'textid': idx, 'text': row['text'], 'label': label_mapping[row['labels']]})
-    idx += 1
-    rows_to_add.append({'textid': idx, 'text': row['text_hi'], 'label': label_mapping[row['labels']]})
-    idx += 1
-    rows_to_add.append({'textid': idx, 'text': row['text_de'], 'label': label_mapping[row['labels']]})
-    idx += 1
-    rows_to_add.append({'textid': idx, 'text': row['text_fr'], 'label': label_mapping[row['labels']]})
-    idx += 1
 
-df_final = pd.concat([df_final, pd.DataFrame(rows_to_add)], ignore_index=True)
+labels = {'ham': 0, 'spam':1}
 
-df_final.to_csv('multi_lingual.tsv', sep='\t', index=False)
+for idx, row in reference_df.iterrows():
+    for condition in ['text', 'text_hi', 'text_de', 'text_fr']:
+        rows_to_add.append({
+            'textid': idx,
+            'text': row[condition],
+            'target': labels[row['labels']],
+            'condition': condition
+        })
+
+df_final = pd.DataFrame(rows_to_add)
+
+df_final.to_csv('data.tsv', sep='\t', index=False)
+
